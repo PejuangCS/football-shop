41050e39a932d7a8e2231eeba1cd5042755d2a6d
PejuangCS <ilhamalki33@gmail.com>
2025-09-07 10:50:35 +0700

    routing urls.py

---
+++ main/models.py
@@ -0,0 +1,31 @@
+import uuid
+from django.db import models
+
+class Product(models.Model):
+    CATEGORY_CHOICES = [
+        ('transfer', 'Transfer'),
+        ('update', 'Update'),
+        ('exclusive', 'Exclusive'),
+        ('match', 'Match'),
+        ('rumor', 'Rumor'),
+        ('analysis', 'Analysis'),
+    ]
+    
+    name = models.CharField(max_length=255)
+    price = models.PositiveIntegerField(default=0)
+    description = models.TextField()
+    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES, default='update')
+    thumbnail = models.URLField(blank=True, null=True)
+    stock = models.PositiveIntegerField(default=0)
+    is_featured = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.title
+    
+    @property
+    def is_news_hot(self):
+        return self.news_views > 20
+        
+    def increment_views(self):
+        self.news_views += 1
+        self.save()
